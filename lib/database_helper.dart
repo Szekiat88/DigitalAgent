9ae66c7f20d1ae29441e5838a85d8b6aa3cb865b
Szekiat <111328882+Szekiat@users.noreply.github.com>
2023-03-25 08:04:07 +0800

    First Commit

---
+++ lib/database_helper.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "mydatabase.db";
+  static final _databaseVersion = 1;
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, _databaseName);
+    return await openDatabase(path, version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE mytable (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        value INTEGER
+      )
+    ''');
+  }
+}
